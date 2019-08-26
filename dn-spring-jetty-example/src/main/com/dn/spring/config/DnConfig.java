566c0e548cb8309a203aae7dd93f1666be29c654
Muthukumar Thangavinayagam <muthukumar.wit@gmail.com>
2019-08-26 14:38:30 +0000

    Spring Jetty initial Code Commit

---
+++ dn-spring-jetty-example/src/main/com/dn/spring/config/DnConfig.java
@@ -0,0 +1,12 @@
+package com.dn.spring.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan("{com.dn.spring.controller}")
+public class DnConfig{
+
+}
